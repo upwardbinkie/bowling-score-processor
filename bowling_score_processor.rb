f06108b4c247ed63e55dbd2c1d725db0062e9b19
Harry Jackson <upwardbinkie@gmail.com>
2021-06-20 23:21:40 -0500

    Print pinfalls and score

--- bowling_score_processor.rb
+++ bowling_score_processor.rb
@@ -4,16 +4,16 @@ require_relative 'lib/player'
 game = Game.new
 game_data = game.load_game
 
+print "Frame\t\t"
+
+(1..10).each do |i|
+    print "#{i}\t\t"
+end
+
+print "\n"
+
 game_data.each do |data|
-    prev_score = 0
-
-    print "Frame\t"
-
-    (1..10).each do |i|
-        print "#{i}\t\t"
-    end
-
-    print "\n"
+    prev_pins = 0
 
     puts data.name
     
@@ -26,18 +26,18 @@ game_data.each do |data|
     is_second_roll = false
 
     while roll < pinfalls.length - 1 do
-        score = pinfalls[roll]
-        next_score = pinfalls[roll + 1]        
+        pins = pinfalls[roll]
+        next_pins = pinfalls[roll + 1]        
 
         if frame < 9
-            if score.to_i == 10 # is a strike        
-                frames[frame] = score
+            if pins.to_i == 10 # is a strike        
+                frames[frame] = pins
             else # not a strike         
-                frames[frame] = [score, next_score]
+                frames[frame] = [pins, next_pins]
                 roll+=1
             end
         else
-            frames[frame] = [score, next_score, pinfalls.last]
+            frames[frame] = [pins, next_pins, pinfalls.last]
             roll+=1
         end
 
@@ -45,15 +45,81 @@ game_data.each do |data|
         frame += 1
     end
 
-    print "\n"
-    
-    puts "-------------"
-
+    print "Pinfalls"
     frames.each do |frame|
-        puts "---"
-        print frame
-        print "\n"
+        if frame == "10"
+            print "\t\tX"
+            next
+        end
+
+        if frame.length == 3
+            frame.each do |f|
+                if f == "10"
+                    print "\tX"
+                else
+                    print "\t#{f}"
+                end
+            end
+            next
+        end
+
+        if frame[0].to_i + frame[1].to_i == 10
+            print "\t#{frame[0]}"
+            print "\t/"
+        else
+            print "\t#{frame[0]}"
+            print "\t#{frame[1]}"
+        end
     end
+    
+    print "\n"
+
+    print "Score\t\t"
+    scores = Array.new
+
+    (0..9).each do |i|
+        
+        if(i == 9)
+            scores[i] = frames[i].map(&:to_i).sum
+            next
+        end
+        
+        next_frame = frames[i + 1]
+
+        if frames[i] == "10"
+            if next_frame.kind_of?(Array)
+                scores[i] = 10 + next_frame[0].to_i + next_frame[1].to_i
+            else
+                if frames[i + 2].kind_of?(Array)
+                    scores[i] = 10 + 10 + frames[i + 2][0].to_i
+                else
+                    scores[i] = 10 + 10 + 10
+                end
+            end
+            next
+        end
+
+        current_frame_score = frames[i][0].to_i + frames[i][1].to_i
+
+        if current_frame_score == 10
+            if next_frame.kind_of?(Array)
+                scores[i] = 10 + next_frame[0].to_i
+            else
+                scores[i] = 10 + 10
+            end
+            next
+        end
+
+        scores[i] = current_frame_score
+    end
+
+    score = 0
+    scores.each do |s|
+        score += s
+        print "#{score}\t\t"
+    end
+    
+    print "\n"
 
 end
 
